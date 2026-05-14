76c71430a43d3ea29836a982feba81a7784623e9
Gabriel Aurelio Gipp <gabrielaureliogipp@MacBook-Air-de-Gabriel-2.local>
2026-05-14 16:23:08 -0300

    feat(lead-detail): add barrel exports

---
+++ src/components/lead-detail/index.ts
@@ -0,0 +1,3 @@
+export { LeadDetailSheet } from "./LeadDetailSheet";
+export { LeadPanelProvider, useLeadSheet } from "./hooks/useLeadSheet";
+export type { DrawerVariant } from "@/components/leads/LeadDetailDrawer";
